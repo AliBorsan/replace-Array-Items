7f0d4ed0f61fb61c9582caa275ba47ae63313c9d
AliBorsan <80667402+AliBorsan@users.noreply.github.com>
2024-08-16 22:15:32 +0200

    Update a.js

--- a.js
+++ a.js
@@ -2,10 +2,15 @@
 let vendors = ['hp', 'Samsung', 'Dell', 'apple' , 'hp', 'Huawei'];
 console.log(vendors);
 //Array(6) ['hp', 'Samsung', 'Dell', 'apple' , 'hp', 'Huawei']
+
 let customers = ['Sam', 'John', 'xi', 'Albert' , 'xi', 'jurt', 'xi'];
 console.log(customers);
 //Array(6) ['Sam', 'John', 'xi', 'Albert' , 'xi', 'jurt', 'xi']
 
+//A function to replace an item or more items in an array
+//@param : arr: The source arry
+//@param : item: The item to be replaced
+//@param : val: The new item to replace (item)
 
 function replaceArrayItems(arr, item, val){
  
@@ -13,12 +18,11 @@ function replaceArrayItems(arr, item, val){
     for(let i = 0; i <= len; i++){
         if(arr[i] === item){
             arr[i] = val;
-
         }
     }
-	  
   }
 
+//Usage :
 replaceArrayItems(vendors, 'hp', 'Xiaomi');
 console.log(vendors);
 //Array(6) [ "Xiaomi", "Samsung", "Dell", "apple", "Xiaomi", "Huawei" ]
